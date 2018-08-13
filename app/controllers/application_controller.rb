f5921e1a56109b6c60f2f91bc1456fe0a970382e
Ishani Amin <ishaniamin@gmail.com>
2018-08-13 17:56:34 +0000

    added template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,27 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+  
+  post '/results' do
+    #puts params
+    points_array = params.values # makes an array that has all the point values from the questions answered
+    total = 0
+    points_array.each do |point| # loop through points array to calculate total
+      total += point.to_i # makes sure to convert points to an integer because it's a string
+    end
+    #puts total
+    
+    result = calculate_result(total) # use the method calculate_result (that we defined in sample_model.rb) to get result
+    
+    #puts result
+    if result == "bffs" # depending on the result, redirect to a certain erb file (aka different page)
+      return erb :bff
+    elsif result == "ok"
+      return erb :okay
+    else
+      return erb :nope
+    end
+    
+    
+  end
 end
